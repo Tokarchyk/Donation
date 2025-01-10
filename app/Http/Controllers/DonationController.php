b233977f361b5cfe104317a7428dedd8777a6ea1
thibylko <thibylko1993@gmail.com>
2025-01-10 14:42:59 +0200

    update error

--- app/Http/Controllers/DonationController.php
+++ app/Http/Controllers/DonationController.php
@@ -8,8 +8,8 @@ use App\Models\Donation;
 
 class DonationController extends Controller
 {
-    public function index(): View{
-        
+    public function index(): View
+    {
         $donations = Donation::paginate(10);
 
         return view('dashboard', compact("donations"));
--- app/Models/Donation.php
+++ app/Models/Donation.php
@@ -8,6 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Donation extends Model
 {
     use HasFactory;
-
 }
-
