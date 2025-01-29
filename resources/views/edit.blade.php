57dd847698c7368628497985b2c27768b43227fc
thibylko <thibylko1993@gmail.com>
2025-01-29 10:57:45 +0200

    Update edit.blade.php

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -2,41 +2,41 @@
 @section('content')
 
 <form class="w-50 mt-5 m-auto" action="{{ route('donations.update', $donation) }}" method ="POST">
-        @csrf
-        @method('PUT')
-        <div class="mb-3">
-                <label for="name" class="form-label ">Name</label>
-                <input type="name" class="form-control" id="name" 
-                        name="donator_name" aria-describedby="name" 
-                        value="{{ $donation->donator_name }}">
-                @error('donator_name')
-                        <div class="error-message">{{ $message }}</div>
-                @enderror
-        </div>
-        <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" 
-                        name="email" aria-describedby="emailHelp"
-                        value="{{ $donation->email }}">
-                @error('email')
-                        <div class="error-message">{{ $message }}</div>
-                @enderror
-        </div>
-        <div class="mb-3">
-                <label for="amount" class="form-label">Amount</label>
-                <input type="sum" class="form-control" id="amount" 
-                        name="amount" 
-                        value="{{ $donation->amount }}">
-                @error('amount')
-                        <div class="error-message">{{ $message }}</div>
-                @enderror
-        </div>
-        <div class="mb-3">
-                <label for="text" class="form-label">Message</label>
-                <input type="text" class="form-control" id="text" 
-                        name="message" 
-                        value="{{ $donation->message }}">
-        </div>
-        <button type="submit" class="btn btn-primary">Save update</button>
+    @csrf
+    @method('PUT')
+    <div class="mb-3">
+        <label for="name" class="form-label ">Name</label>
+        <input type="name" class="form-control" id="name" 
+        name="donator_name" aria-describedby="name" 
+        value="{{ $donation->donator_name }}">
+        @error('donator_name')
+        <div class="error-message">{{ $message }}</div>
+        @enderror
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" 
+        name="email" aria-describedby="emailHelp"
+        value="{{ $donation->email }}">
+        @error('email')
+        <div class="error-message">{{ $message }}</div>
+        @enderror
+    </div>
+    <div class="mb-3">
+        <label for="amount" class="form-label">Amount</label>
+        <input type="sum" class="form-control" id="amount" 
+        name="amount" 
+        value="{{ $donation->amount }}">
+        @error('amount')
+        <div class="error-message">{{ $message }}</div>
+        @enderror
+    </div>
+    <div class="mb-3">
+        <label for="text" class="form-label">Message</label>
+        <input type="text" class="form-control" id="text" 
+        name="message" 
+        value="{{ $donation->message }}">
+    </div>
+    <button type="submit" class="btn btn-primary">Save update</button>
 </form>
 @endsection
